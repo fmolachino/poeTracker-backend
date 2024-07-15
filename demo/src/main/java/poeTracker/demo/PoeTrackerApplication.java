47460727fe7f5e7adab645e1696ce2e3d102cc76
Fer <fmolachino@gmail.com>
2024-07-15 20:09:16 -0300

    Initial commit

---
+++ demo/src/main/java/poeTracker/demo/PoeTrackerApplication.java
@@ -0,0 +1,13 @@
+package poeTracker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeTrackerApplication.class, args);
+	}
+
+}
